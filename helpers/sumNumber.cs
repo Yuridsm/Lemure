edfab4fdf173347aea9b8f9cebc740a2975a9bf3
yuri.melo <yuri.melo@INTELITRADER-VD>
2020-11-27 12:00:25 -0300

    My own helpers

---
+++ helpers/sumNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.helpers
+{
+    class sumNumber
+    {
+        public static int Sum2Number(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
