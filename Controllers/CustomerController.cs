f16eefad9867181b7c1560b39ccf52e5b4e85662
yuri.melo <yuri.melo@INTELITRADER-VD>
2020-11-27 11:59:03 -0300

    My app's Controllers

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeanringWithMosh.helpers;
+
+namespace LeanringWithMosh.Controllers
+{
+    class CustomerController
+    {
+        public static void message()
+        {
+            Console.WriteLine(sumNumber.Sum2Number(2,2));
+            Console.WriteLine("Testing...");
+        }
+        
+    }
+}
