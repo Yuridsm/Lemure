d92dc91a74c4af2e9d6a265972c847cc8310e6ed
yuri.melo <yuri.melo@INTELITRADER-VD>
2020-11-27 12:00:01 -0300

    Core of the Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LeanringWithMosh.Controllers;
+
+namespace LeanringWithMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CustomerController.message();
+        }
+    }
+}
