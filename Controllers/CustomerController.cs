22c0343bc62dae0b5d9edc08e1cadaa7f3d762a6
Yuri Melo <yurifullstack@gmail.com>
2020-11-28 17:57:32 -0300

    Setting up database services

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using LeanringWithMosh.helpers;
+using LeanringWithMosh.interfaces;
 
 namespace LeanringWithMosh.Controllers
 {
     class CustomerController
     {
+        private readonly IDatabase _databaseConnect;
+
+        public CustomerController(IDatabase databaseConnect)
+        {
+            this._databaseConnect = databaseConnect;
+        }
+
         public static void message()
         {
             Console.WriteLine(sumNumber.Sum2Number(2,2));
             Console.WriteLine("Testing...");
+        }
+
+        public IDatabase getRepository()
+        {
+            return this._databaseConnect;
         }
         
     }
