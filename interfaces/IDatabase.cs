5a9315dd690ca22303ac9ec4253015b2fcbab04e
Yuri Melo <yurifullstack@gmail.com>
2020-11-28 17:57:48 -0300

    Interface that database services must be implementing

---
+++ interfaces/IDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.interfaces
+{
+    interface IDatabase
+    {
+        public void insert();
+        public void delete();
+        public void select();
+        public void update();
+
+    }
+}
