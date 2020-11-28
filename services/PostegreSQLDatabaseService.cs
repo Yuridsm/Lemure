e0554ad9937cec901158888f34666fedb17cbf1c
Yuri <yurifullstack@gmail.com>
2020-11-28 18:04:02 -0300

    PostegreSQL Service

---
+++ services/PostegreSQLDatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeanringWithMosh.interfaces;
+
+namespace LeanringWithMosh.services
+{
+    class PostegreSQLDatabaseService : IDatabase
+    {
+        public void delete()
+        {
+            Console.WriteLine("PostegreSQL...");
+            Console.WriteLine("Deleting 1 row");
+        }
+
+        public void insert()
+        {
+            Console.WriteLine("PostegreSQL...");
+            Console.WriteLine("Inserting 1 row");
+        }
+
+        public void select()
+        {
+            Console.WriteLine("PostegreSQL...");
+            Console.WriteLine("Selecting 1 row");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("PostegreSQL...");
+            Console.WriteLine("Updating 1 row");
+        }
+    }
+}
