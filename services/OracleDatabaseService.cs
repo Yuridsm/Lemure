b51c877c4c5c7d5b9d5aca74e0f11e0a335e6411
Yuri <yurifullstack@gmail.com>
2020-11-28 18:03:29 -0300

    Oracle Service

--- services/OracleDatabaseService.cs
+++ services/OracleDatabaseService.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using LeanringWithMosh.interfaces;
 
 namespace LeanringWithMosh.services
 {
-    class OracleDatabaseService
+    class OracleDatabaseService : IDatabase
     {
+        public void delete()
+        {
+            Console.WriteLine("Oracle...");
+            Console.WriteLine("Deleting 1 row");
+        }
+
+        public void insert()
+        {
+            Console.WriteLine("Oracle...");
+            Console.WriteLine("Inserting 1 row");
+        }
+
+        public void select()
+        {
+            Console.WriteLine("Oracle...");
+            Console.WriteLine("Selecting 1 row");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("Oracle...");
+            Console.WriteLine("Updating 1 row");
+        }
     }
 }
