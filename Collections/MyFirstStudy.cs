9ef7f356ea7e6afd5257031451bd77d3e12115e1
Yuri <yurifullstack@gmail.com>
2020-12-20 13:09:09 -0300

    Collections

---
+++ Collections/MyFirstStudy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.Collections
+{
+    class MyFirstStudy
+    {
+        public static void LoadProducts()
+        {
+            
+        }
+    }
+}
