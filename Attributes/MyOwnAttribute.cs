a67d0367198f1d57cc113fad8f8ea477a3df0d53
Yuri <yurifullstack@gmail.com>
2020-12-20 13:08:39 -0300

    Creating y own attributes

---
+++ Attributes/MyOwnAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeanringWithMosh.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited= true, AllowMultiple = true)]
+    class MyOwnAttribute: Attribute
+    {
+        private bool myValue;
+        public MyOwnAttribute(bool myValues)
+        {
+            this.myValue = myValues;
+        }
+    }
+}
---
+++ Attributes/UsingAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.Attributes
+{
+    class UsingAttributes
+    {
+        [Obsolete("Will be removed in next version")]
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
