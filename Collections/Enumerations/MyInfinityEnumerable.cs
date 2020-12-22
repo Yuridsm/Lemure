fe4d709d7783988404572a056d293a2d396daf1e
Yuri <yurifullstack@gmail.com>
2020-12-22 17:56:44 -0300

    My Collection

---
+++ Collections/Enumerations/MyInfinityEnumerable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeanringWithMosh.Collections.Enumerations
+{
+    public class MyInfinityEnumerable : IEnumerable<int>
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return new MyInfinityEnumerator();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            return new MyInfinityEnumerator();
+        }
+    }
+
+    public class MyInfinityEnumerator : IEnumerator<int>
+    {
+        public object Current { get, private set; } = 0;
+
+        int IEnumerator<int>.Current => throw new System.NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
---
+++ Collections/LookingForInList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeanringWithMosh.Collections
+{
+    static class LookingForInList
+    {
+        /*
+            You need to be able to either retrieve or count the number of occurrences of an object
+            contained in a List<T> that matches a search criterion.
+         */
+        
+            
+    }
+}
