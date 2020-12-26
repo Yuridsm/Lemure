bcb8cfaa4c00dc9c3e9f7c942fcb40dca2da62b4
Yuri <yurifullstack@gmail.com>
2020-12-26 19:55:51 -0300

    New alterations

--- Program.cs
+++ Program.cs
@@ -1,33 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using LeanringWithMosh.LyingAround;
+using System;
 
 namespace LeanringWithMosh
 {
-    
     class Program
     {
         static void Main(string[] args)
         {
-            /*Person[] peopleArray = new Person[3] 
-            {
-                new Person("John", "Smith"),
-                new Person("Jim", "Johnson"),
-                new Person("Sue", "Rabon")
-            };
-
-            People peopleList = new People(peopleArray);
-            foreach (Person p in peopleList)
-                Console.WriteLine(p.firstName + " " + p.lastName);*/
-            var names = new List<string> { "sam", "alexia", "simon", "sumanth", "tony", "sam", "amr", "mark", "drew" };
-            var moreThenFiveLetters = names.Where(w => w.Length > 5);
-            names[0] = "benjamin";
-
-            foreach(var name in moreThenFiveLetters)
-            {
-                Console.WriteLine(name);
-            }
-
+            KGB kgb = new KGB();
+            Console.WriteLine(kgb.Info());
         }
     }
 }
