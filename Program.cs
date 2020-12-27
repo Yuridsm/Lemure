855579f9322765cc072ab4c874c276bc2d91bbaf
Yuri <yurifullstack@gmail.com>
2020-12-27 12:02:51 -0300

    Studying IDiposable and IComparable interfaces

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using LeanringWithMosh.LyingAround;
+﻿using LeanringWithMosh.interfaces.Comparable;
+using LeanringWithMosh.interfaces.Disposable;
+using System.Collections.Generic;
 using System;
+using System.Text;
 
 namespace LeanringWithMosh
 {
@@ -7,8 +10,28 @@ namespace LeanringWithMosh
     {
         static void Main(string[] args)
         {
-            KGB kgb = new KGB();
-            Console.WriteLine(kgb.Info());
+            List<Employee> list = new List<Employee>();
+            list.Add(new Employee() { name = "Macoratti", salary = 80000, cpf = new CPF() { cpf = 11905365446 } });
+            list.Add(new Employee() { name = "Janice", salary = 10000, cpf = new CPF() { cpf = 20005365446 } });
+            list.Add(new Employee() { name = "Yuri", salary = 40000, cpf = new CPF() { cpf = 13005365446 } });
+            list.Add(new Employee() { name = "Bianca", salary = 50000, cpf = new CPF() { cpf = 80005365446 } });
+            list.Add(new Employee() { name = "Amanda", salary = 17000, cpf = new CPF() { cpf = 97805365446 } });
+            list.Add(new Employee() { name = "Jefferson", salary = 8000, cpf = new CPF() { cpf = 70205365446 } });
+            list.Add(new Employee() { name = "Raiela", salary = 90000, cpf = new CPF() { cpf = 70105365446 } });
+            list.Sort();
+
+            foreach(var employee in list)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.ReadLine();
+
+            // Training IDisposable
+            using (FilesManager manager = new FilesManager("C:\\Users\\yurii\\Documents\\yuriCSharp.data"))
+            {
+                byte[] data = Encoding.ASCII.GetBytes("C:\\Users\\yurii\\Documents\\Macoratti.net");
+                manager.Write(data);
+            }
         }
     }
 }
