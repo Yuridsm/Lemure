fff5eb1ebf5e78a8e0e21fb2418976ae31a8ce45
Yuri <yurifullstack@gmail.com>
2020-12-27 12:05:17 -0300

    Studying IComparable interface

---
+++ interfaces/Comparable/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeanringWithMosh.interfaces.Comparable
+{
+    public class Employee : IComparable<Employee>
+    {
+        public string name { get; set; }
+        public int salary { get; set; }
+        public CPF cpf { get; set; }
+        // Estou ordenando apenas por CPF
+        public int CompareTo(Employee epl)
+        {
+            return this.cpf.CompareTo(epl.cpf);   
+        }
+        public override string ToString()
+        {
+            return this.salary.ToString() + ", " + this.name + " : [" + this.cpf.ToString() + "]";
+        }
+    }
+}
