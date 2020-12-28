ac3e0ec660bc8e54f8e75107cb3036d0b0400274
Yuri <yurifullstack@gmail.com>
2020-12-27 23:37:36 -0300

    MyDatabase class for testing the IDisposable

---
+++ interfaces/Disposable/MyDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.interfaces.Disposable
+{
+    public class MyDatabase : IDisposable
+    {
+        private string _connectionString;
+        public MyDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void GetOnDatabase()
+        {
+            Console.WriteLine("Connecting to the Database by means of {0}", this._connectionString);
+            Console.WriteLine("Getting data...");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Close my connection by dispose method.");
+        }
+    }
+}
