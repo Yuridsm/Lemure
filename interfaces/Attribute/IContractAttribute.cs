add1b8cb07a118885226ea14e8d5f53362505288
Yuri <yurifullstack@gmail.com>
2021-01-02 14:21:47 -0300

    Interface fo attribute

---
+++ interfaces/Attribute/IContractAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.interfaces.Attribute
+{
+    public interface IContractAttribute
+    {
+        public string firstName { get; }
+        public string lastName { get; }
+        public string age { get; }
+        public bool assign { get; }
+    }
+}
