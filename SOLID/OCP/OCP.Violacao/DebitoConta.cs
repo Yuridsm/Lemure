393b8392d0b565cef0ce5160ff9ea02c96b845be
Yuri <yurifullstack@gmail.com>
2021-01-03 22:17:26 -0300

    OCP from SOLID

--- SOLID/OCP/OCP.Violacao/DebitoConta.cs
+++ SOLID/OCP/OCP.Violacao/DebitoConta.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LeanringWithMosh.SOLID.OCP.OCP.Violacao
+﻿namespace LeanringWithMosh.SOLID.OCP.OCP.Violacao
 {
     public class DebitoConta
     {
