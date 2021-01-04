8aed7ae3be12e1fd0aa7e26f893c414a9dc2d168
Yuri <yurifullstack@gmail.com>
2021-01-03 22:18:29 -0300

    Writing my own code by appliying SOLID concepts

---
+++ SOLID/OCP/MyOwnCode/DebitoConta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.SOLID.OCP.MyOwnCode
+{
+    public abstract class DebitoConta
+    {
+        private int numeroDaTransacao { get; }
+
+        public abstract string Debitar(decimal valor, string conta);
+        public static string CodeGenerate()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ SOLID/OCP/MyOwnCode/DebitoContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.SOLID.OCP.MyOwnCode
+{
+    class DebitoContaCorrente
+    {
+    }
+}
