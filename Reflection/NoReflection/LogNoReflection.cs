735883c32d602d3c4a0a804e64e45ee285daec21
Yuri <yurifullstack@gmail.com>
2021-01-07 19:06:38 -0300

    Learning Reflection

---
+++ Reflection/NoReflection/LogNoReflection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace LeanringWithMosh.Reflection.NoReflection
+{
+    public class LogNoReflection
+    {
+        public static void ProductLog(Product product)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Product log");
+            builder.AppendLine("Date: " + DateTime.Now);
+            builder.AppendLine("Id: " + product.Id);
+            builder.AppendLine("Name: " + product.Name);
+            builder.AppendLine("Description: " + product.Description);
+            builder.AppendLine("Stock: " + product.Stock);
+            LogPrint(builder.ToString());
+        }
+
+        public static void OrdersLog(Order order)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Order log");
+            builder.AppendLine("Date: " + DateTime.Now);
+            builder.AppendLine("Id: " + order.Id);
+            builder.AppendLine("CustumerId: " + order.CustumerId);
+            builder.AppendLine("OrderDate: " + order.OrderDate);
+            LogPrint(builder.ToString());
+        }
+
+        public static void CustomerLog(Customer customer)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Order log");
+            builder.AppendLine("Date: " + DateTime.Now);
+            builder.AppendLine("Id: " + customer.Id);
+            builder.AppendLine("Name: " + customer.Name);
+            builder.AppendLine("Address: " + customer.Address);
+            LogPrint(builder.ToString());
+        }
+
+        public static void LogPrint(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Reflection/NoReflection/Order.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace LeanringWithMosh.Reflection.NoReflection
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustumerId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
