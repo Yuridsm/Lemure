cc9c02b7a6ed5d0a43f04973a8e4cce3185663fa
Yuri <yurifullstack@gmail.com>
2021-01-08 23:54:45 -0300

    REBASE: Lazy Initialization

---
+++ LazyInitialization/LazyInit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeanringWithMosh.LazyInitialization;
+
+namespace LeanringWithMosh.LazyInitialization
+{
+    class LazyInit
+    {
+        public void Load()
+        {
+            Lazy<List<Order>> _orders = new Lazy<List<Order>>(() => new Order().create(3));
+            Console.WriteLine(_orders.Value.Count);
+        }
+    }
+}
