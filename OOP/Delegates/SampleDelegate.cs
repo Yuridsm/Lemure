589c82a024ea1769c86b0f46749da657bb9ee540
Yuri <yurifullstack@gmail.com>
2021-01-30 17:34:44 -0300

    Delegate

---
+++ OOP/Delegates/SampleDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.OOP.Delegates
+{
+    public class SampleDelegate
+    {
+        public delegate String myMethodDelegate(int myInt);
+
+        public class mySampleClass
+        {
+            public String myStringMethod(int myInt)
+            {
+                if(myInt > 0)
+                    return ("positive");
+                if(myInt < 0)
+                    return ("negative");
+                return ("zero");
+            }
+
+            public static String mySignMethod(int myInt)
+            {
+                if (myInt > 0)
+                    return ("+");
+                if(myInt > 0)
+                    return ("+");
+                return ("");
+            }
+        }
+    }
+}
