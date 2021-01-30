329719f2a51deeec3ce4801e651fa39260474dfa
Yuri <yurifullstack@gmail.com>
2021-01-30 17:33:58 -0300

    Interface: Agent

---
+++ LyingAround/IAgent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.LyingAround
+{
+    public interface IAgent<T> : ICode
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int age { get; set; }
+
+        public void GetInfo();
+    }
+}
