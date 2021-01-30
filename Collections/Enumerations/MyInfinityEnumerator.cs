151c3a3620807556ac3d53be79284f1add68f484
Yuri <yurifullstack@gmail.com>
2021-01-30 17:33:17 -0300

    Enumerations

---
+++ Collections/Enumerations/MyInfinityEnumerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.Collections.Enumerations
+{
+    public class MyInfinityEnumerator : IEnumerator<Product>
+    {
+        public Product[] _products { get; set; }
+        private int index = -1;
+
+        public MyInfinityEnumerator(Product[] products)
+        {
+            _products = products;
+        }
+
+        public Product Current {
+            get => _products[index];
+        }
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < _products.Length;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
