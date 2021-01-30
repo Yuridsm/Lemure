e0781f1ce7ff939b6be45f5eba2192e84d71e317
Yuri <yurifullstack@gmail.com>
2021-01-30 17:34:35 -0300

    Delegate

---
+++ OOP/Delegates/PlayFavoriteMusic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.OOP.Delegates
+{
+    public class PlayFavoriteMusic
+    {
+        public String play(string music)
+        {
+            if(music == "Ela vem".ToLower())
+                return ("Ela vem - Biu do Piseiro...");
+            return ("Não há música na playlist");
+        }
+
+        public static String next()
+        {
+            return ("Virgem - Biu do Piseiro...");
+        }
+    }
+}
