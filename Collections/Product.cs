c2941910a223bfc4a1e1e97d23f9acbd5f465471
Yuri <yurifullstack@gmail.com>
2021-01-30 17:33:24 -0300

    Product model

---
+++ Collections/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeanringWithMosh.Collections
+{
+    public class Product
+    {
+        public string _name { get; set; }
+        public string _code { get; }
+        public string _description { get; set; }
+
+        public Product(string name, string code, string description)
+        {
+            _name = name;
+            _code = code;
+            _description = description;
+        }
+        public override string ToString()
+        {
+            return $"Name: {_name} - Code: {_code} - Description: {_description}";
+        }
+    }
+}
