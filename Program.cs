1d69a2985b0241e5a5f524f25ce733811e561451
Yuri <yurifullstack@gmail.com>
2021-01-30 23:56:10 -0300

    Testing Enums and their extensions methods

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using LeanringWithMosh.Collections.Enumerations;
-using LeanringWithMosh.Collections;
-using LeanringWithMosh.OOP.Delegates;
+﻿using LeanringWithMosh.Enums;
 using System;
-using static LeanringWithMosh.OOP.Delegates.SampleDelegate;
-using System.Collections.Generic;
-using System.Linq;
+using LeanringWithMosh.Struct;
 
 namespace LeanringWithMosh
 {
@@ -12,32 +8,27 @@ namespace LeanringWithMosh
     {
         private delegate String myDelegateForMusic1(string music);
         private delegate String myDelegateForMusic2();
+
+        private static void MutateAndDisplay(MutablePoint p)
+        {
+            p.X = 100;
+            Console.WriteLine($"Point mutated in a method: {p}");
+        }
+
         public static void Main(string[] args)
         {
-            mySampleClass smp = new mySampleClass();
-            myMethodDelegate myD1 = new myMethodDelegate(smp.myStringMethod);
-            myMethodDelegate myD2 = new myMethodDelegate(mySampleClass.mySignMethod);
+            MyOptions g1 = MyOptions.D; // 1
+            MyOptions g2 = MyOptions.F; // 0
+            MyOptions g3 = MyOptions.A; // 4
 
-            /*Console.WriteLine("{0} is {1}; use the sign \"{2}\".", 5, myD1(5), myD2(5));
-            Console.WriteLine("{0} is {1}; use the sign \"{2}\".", -3, myD1(-3), myD2(-3));
-            Console.WriteLine("{0} is {1}; use the sign \"{2}\".", 0, myD1(0), myD2(0));*/
+            Console.WriteLine("First {0} a passing grade.", g1.Passing() ? "is" : "is not");
+            Console.WriteLine("Second {0} a passing grade.", g2.Passing() ? "is" : "is not");
 
-            PlayFavoriteMusic music = new PlayFavoriteMusic();
-            myDelegateForMusic1 music1 = new myDelegateForMusic1(music.play);
-            myDelegateForMusic2 music2 = new myDelegateForMusic2(PlayFavoriteMusic.next);
-
-            Console.WriteLine("Which music is playing?");
-            Console.WriteLine(music1("ela vem"));
-            Console.WriteLine("Next music is :");
-            Console.WriteLine(music2());
-            Console.WriteLine(music1.GetInvocationList());
-            Console.WriteLine(FuncDelegate.selector("YURI MELOOOO"));
-            // ---
-            Func<string, string> selector = str => str.ToUpper();
-            string[] name = { "yuri melo", "developer", "23 years old" };
-            IEnumerable<String> words = name.Select(selector);
-            foreach (String a in words)
-                Console.WriteLine(a);
+            EnumExtension.minPassing = MyOptions.C; // 2
+            Console.WriteLine("\r\nRaising the bar!\r\n");
+            Console.WriteLine("First {0} a passing grade.", g1.Passing() ? "is" : "is not");
+            Console.WriteLine("Second {0} a passing grade.", g2.Passing() ? "is" : "is not");
+            Console.WriteLine("Second {0} a passing grade.", g3.Passing() ? "is" : "is not");
         }
     }
 }
