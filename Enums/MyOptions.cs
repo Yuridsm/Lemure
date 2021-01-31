ea8d749d53aa183bcc45aae0e4c906e48287417d
Yuri <yurifullstack@gmail.com>
2021-01-30 23:55:18 -0300

    Studying Enums and Extension method applied to Enums

---
+++ Enums/MyOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeanringWithMosh.Enums
+{
+    public enum MyOptions : int
+    {
+        F = 0,
+        D = 1,
+        C = 2,
+        B = 3,
+        A = 4
+    }
+}
