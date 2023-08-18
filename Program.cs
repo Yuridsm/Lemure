712236f80b722f3ad28285e17df46fe4abfdfff5
Alfredo Melo <yurifullstack@gmail.com>
2023-08-18 13:09:45 -0300

    New changes

--- Program.cs
+++ Program.cs
@@ -1,25 +1,52 @@
 ﻿using System;
-using System.Reflection;
-using Lemure.DesignPatterns.CQRS.Commands;
-using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
-namespace Lemure
+namespace Lemure;
+
+public static class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main(string[] args)
-        {
-            var serviceCollection = new ServiceCollection()
-                .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()))
-                .BuildServiceProvider();
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
 
-            var mediator = serviceCollection.GetRequiredService<IMediator>();
+        var service = new ServiceCollection();
+        service.AddFoo(configuration);
 
-            var command = new CadastraPessoaCommand();
+        var provider = service.BuildServiceProvider();
 
-            var notification = mediator.Send(command);
-            Console.WriteLine(notification);
-        }
+        var controller = new TestController(provider.GetService<IOptions<Baz>>());
+
+        controller.Show();
     }
+
+    public static void AddFoo(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.Configure<Baz>(configuration.GetSection("Baz"));
+    }
+}
+
+public class TestController
+{
+    private readonly Baz options;
+
+    public TestController(IOptions<Baz> options)
+    {
+        this.options = options.Value;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(options.TestXPTO);
+        Console.WriteLine(options.DescriptionXPTO);
+    }
+}
+
+public class Baz
+{
+    public string TestXPTO { get; set; } = string.Empty;
+    public string DescriptionXPTO { get; set; } = string.Empty;
 }
